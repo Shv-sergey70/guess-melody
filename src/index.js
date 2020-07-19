853b052a0d0d2c256fd12acd0f56416a24d1942a
Sergey Shershnev <shv.sergey70@gmail.com>
2020-07-19 13:53:38 +0700

    created reducer & wrapped App into Provider

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './reducer/reducer';
 import App from './components/app/app';
 
 import questions from './mocks/questions';
 
 const init = (gameQuestions) => {
   ReactDom.render(
-      <App
-        time={5}
-        attempts={3}
-        questions={gameQuestions}
-      />,
+      <Provider store={createStore(reducer)}>
+        <App
+          time={5}
+          attempts={3}
+          questions={gameQuestions} />
+      </Provider>,
       document.querySelector(`#root`)
   );
 };
---
+++ src/reducer/reducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+  mistakes: 0,
+  step: -1
+};
+
+const reducer = (state = initialState, action) => {
+  return state;
+};
+
+export default reducer;
