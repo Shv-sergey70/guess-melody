8a1a9fd9e1c4971a672c0b5101df3a5d78c7859c
Sergey Shershnev <shv.sergey70@gmail.com>
2020-07-24 14:05:17 +0700

    added tests for async opertaions

--- src/reducer/reducer.test.js
+++ src/reducer/reducer.test.js
@@ -1,9 +1,12 @@
-import {ActionCreator, reducer, isGenreAnswerCorrect, isArtistAnswerCorrect} from './reducer';
+import createAPI from '../api';
+import MockAdapter from 'axios-mock-adapter';
+import {ActionCreator, reducer, isGenreAnswerCorrect, isArtistAnswerCorrect, Operations} from './reducer';
 
 const initialState = {
   mistakes: 0,
   step: -1,
-  time: 300
+  time: 300,
+  questions: []
 };
 
 describe(`Reducer works correctly`, () => {
@@ -52,7 +55,8 @@ describe(`Reducer works correctly`, () => {
       expect(reducer({
         step: 200,
         mistakes: 150,
-        time: 800
+        time: 800,
+        questions: [1, 4, 7]
       }, {
         type: `RESET`
       })).toEqual(initialState);
@@ -72,6 +76,17 @@ describe(`Reducer works correctly`, () => {
         type: `DECREMENT_TIME`,
         payload: 0
       })).toEqual(initialState);
+    });
+  });
+
+  describe(`Reducer with LOAD_QUESTIONS action correctly works`, () => {
+    test(`Payload should be written into questions state`, () => {
+      const questions = [1, 2, 3];
+
+      expect(reducer(initialState, {
+        type: `LOAD_QUESTIONS`,
+        payload: questions
+      })).toEqual(Object.assign({}, initialState, {questions}));
     });
   });
 });
@@ -144,6 +159,28 @@ describe(`ActionCreator correctly works`, () => {
       });
     });
   });
+
+  describe(`requireAuthorization correctly works`, () => {
+    test(`Returns correct object`, () => {
+      const status = true;
+
+      expect(ActionCreator.requireAuthorization(status)).toEqual({
+        type: `REQUIRED_AUTHORIZATION`,
+        payload: status
+      });
+    });
+  });
+
+  describe(`loadQuestions correctly works`, () => {
+    test(`Returns correct object`, () => {
+      const questions = [1, 2, 3];
+
+      expect(ActionCreator.loadQuestions(questions)).toEqual({
+        type: `LOAD_QUESTIONS`,
+        payload: questions
+      });
+    });
+  });
 });
 
 describe(`isGenreAnswerCorrect function correctly works`, () => {
@@ -183,5 +220,45 @@ describe(`isArtistAnswerCorrect function correctly works`, () => {
 
   test(`selected incorrect answer`, () => {
     expect(isArtistAnswerCorrect(`Wrong Artist`, correctAnswer)).toEqual(false);
+  });
+});
+
+describe(`Operations correctly works`, () => {
+  test(`loadQuestions return 200 status, success loading`, () => {
+    const dispatchMock = jest.fn();
+    const API = createAPI(dispatchMock);
+    const APIMock = new MockAdapter(API);
+
+    const mockData = [1, {fake: true}, 3];
+
+    APIMock.onGet(`/questions`).reply(200, mockData);
+    const questionLoader = Operations.loadQuestions();
+
+    return questionLoader(dispatchMock, jest.fn(), API)
+      .then(() => {
+        expect(dispatchMock).toHaveBeenCalledTimes(1);
+        expect(dispatchMock).toHaveBeenCalledWith({
+          type: `LOAD_QUESTIONS`,
+          payload: mockData
+        });
+      });
+  });
+
+  test(`loadQuestions return 403 status, authorization required`, () => {
+    const dispatchMock = jest.fn();
+    const API = createAPI(dispatchMock);
+    const APIMock = new MockAdapter(API);
+
+    APIMock.onGet(`/questions`).reply(403);
+    const questionLoader = Operations.loadQuestions();
+
+    return questionLoader(dispatchMock, jest.fn(), API)
+      .then(() => {
+        expect(dispatchMock).toHaveBeenCalledTimes(2);
+        expect(dispatchMock).toHaveBeenNthCalledWith(1, {
+          type: `REQUIRED_AUTHORIZATION`,
+          payload: true
+        });
+      });
   });
 });
