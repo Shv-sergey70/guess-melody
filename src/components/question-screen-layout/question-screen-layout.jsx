be40916ac4e18f00a9fc44593ca7d4accff854c1
Sergey Shershnev <shv.sergey70@gmail.com>
2020-07-31 16:38:47 +0700

    fixed tests

--- src/components/question-screen-layout/question-screen-layout.jsx
+++ src/components/question-screen-layout/question-screen-layout.jsx
@@ -1,7 +1,6 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
-import Timer from "../timer/timer";
 import MistakesList from "../mistakes-list/mistakes-list";
 import {ActionCreator} from "../../reducer/game/game";
 import {connect} from "react-redux";
